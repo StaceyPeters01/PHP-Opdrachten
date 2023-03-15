aa7f908c24f870533382d33c29f4a82a18d43d61
StaceyPeters01 <9018900@student.zadkine.nl>
2023-03-15 10:46:54 +0100

    crud bieren

--- Ontwerpen/crudbieren/update_bier.php
+++ Ontwerpen/crudbieren/update_bier.php
@@ -5,14 +5,19 @@
 
     echo "Data uit het vorige formulier:<br>";
    // Haal alle info van de betreffende biercode $_GET['biercode']
-   
+   $biercode = $_GET['biercode'];
+   GetBier($biercode);
    ?>
 
 <html>
     <body>
-        <form method="post" action='test.php'>
+        <form method="post">
         <br>
         Biercode:<input type="" name="biercode" value="<?php echo $_GET['biercode'];?>"><br>
+        Naam:<input type="" name="naam" value="<?php echo $row['naam'];?>"><br>
+        Soort:<input type="" name="soort" value="Soort"><br>
+        Stijl:<input type="" name="stijl" value="Stijl"><br>
+        Alcohol:<input type="" name="alcohol" value="Alcohol"><br><br>
         
        
         <input type="submit" name="btn_wzg" value="Wijzigen"><br>
